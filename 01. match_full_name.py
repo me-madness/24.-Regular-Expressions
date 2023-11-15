1fb50cb0f117dab648556e56c689f003e3fa555c
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-15 21:10:42 +0100

    01.finish

--- 01. match_full_name.py
+++ 01. match_full_name.py
@@ -3,8 +3,8 @@
 import re
 
 names = input()
-valid_names = "\b[A-Z][a-z]+ [A-Z][a-z]+\b"
-matches = re.findall(valid_names names)
+valid_names = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b"
+matches = re.findall(valid_names, names)
 print(" ".join(matches))
 
 # Second task from me
