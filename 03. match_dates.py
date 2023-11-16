51c4acdf02465b89626481f94272c9a8672254da
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-16 21:00:10 +0100

    03 it's ready now

--- 03. match_dates.py
+++ 03. match_dates.py
@@ -5,7 +5,9 @@ import re
 dates = input()
 pattern = "\\b(?P<day>\\d{2})([-.\\/])(?P<month>[A-Z][a-z]{2})\\2(?P<year>\\d{4})\\b"
 matches = re.findall(pattern, dates)
-print(" ".join(matches))
+for match in matches:
+    print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
+    
 
 # Second task from me
 
