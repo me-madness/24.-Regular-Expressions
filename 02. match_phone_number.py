93fba74af5107c976ea17a5c299f17f1db00adb8
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-16 20:42:15 +0100

    02. Done by me

--- 02. match_phone_number.py
+++ 02. match_phone_number.py
@@ -1,7 +1,11 @@
 # First task from the lecture
 
+import re
+
 phone_number = input()
 phone_number_regex = "(\+359-2-[0-9]{3}-[0-9]{4}\\b|\+359 2 [0-9]{3} [0-9]{4}\\b)"
+matches = re.findall(phone_number_regex, phone_number)
+print(" ".join(matches)) 
 
 # Second task from me
 
