3a99c5b063b07383ce04740e6f3c227067ae51ac
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-16 21:08:16 +0100

    04 ilso done with this finish the task for this lesson

--- 04. match_numbers.py
+++ 04. match_numbers.py
@@ -1,6 +1,12 @@
 # First task from the lecture
 
-
+import re
+data = input()
+pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+matches = re.finditer(pattern, data)
+for match in matches:
+    print(match.group(), end="")
+    
 
 # Second task from me
 
